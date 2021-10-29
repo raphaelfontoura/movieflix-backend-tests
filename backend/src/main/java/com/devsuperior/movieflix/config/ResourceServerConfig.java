ba05f0a4e395f1e93c0c3c14856d6b77e7696d5c
Raphael Fontoura <raphael.fontoura@gmail.com>
2021-10-29 13:49:00 -0300

    Implement ResourceServerConfig

---
+++ backend/src/main/java/com/devsuperior/movieflix/config/ResourceServerConfig.java
@@ -0,0 +1,38 @@
+package com.devsuperior.movieflix.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+    @Autowired
+    private JwtTokenStore tokenStore;
+
+    private static final String[] PUBLIC = {};
+    private static final String[] PUBLIC_GET = {};
+    private static final String[] AUTHENTICATED = {};
+    private static final String[] ADMIN = {"/users/**"};
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+        resources.tokenStore(tokenStore);
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers(PUBLIC).permitAll()
+                .antMatchers(HttpMethod.GET, PUBLIC_GET).permitAll()
+                .antMatchers(AUTHENTICATED).hasAnyRole("ADMIN", "USER")
+                .antMatchers(ADMIN).hasRole("ADMIN")
+                .anyRequest().authenticated();
+    }
+}
