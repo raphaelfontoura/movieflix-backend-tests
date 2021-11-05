0dfa77bc049ac94c60b595c66999b50b66d10f25
Raphael Fontoura <raphael.fontoura@gmail.com>
2021-11-05 17:08:45 -0300

    Implement MovieResource findAllPaged

--- backend/src/main/java/com/devsuperior/movieflix/services/GenreService.java
+++ backend/src/main/java/com/devsuperior/movieflix/services/GenreService.java
@@ -5,6 +5,7 @@ import com.devsuperior.movieflix.repositories.GenreRepository;
 import com.devsuperior.movieflix.resources.GenreResource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -15,6 +16,7 @@ public class GenreService {
     @Autowired
     private GenreRepository repository;
 
+    @Transactional(readOnly = true)
     public List<GenreDTO> findAll() {
         var genreList = repository.findAll();
         return genreList.stream().map(GenreDTO::new).collect(Collectors.toList());
--- backend/src/main/java/com/devsuperior/movieflix/services/MovieService.java
+++ backend/src/main/java/com/devsuperior/movieflix/services/MovieService.java
@@ -3,7 +3,10 @@ package com.devsuperior.movieflix.services;
 import com.devsuperior.movieflix.dto.MovieDTO;
 import com.devsuperior.movieflix.repositories.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class MovieService {
@@ -11,8 +14,15 @@ public class MovieService {
     @Autowired
     private MovieRepository repository;
 
+    @Transactional(readOnly = true)
     public MovieDTO findById(Long id) {
         var movie = repository.findById(id).orElseThrow();
         return new MovieDTO(movie);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<MovieDTO> findAllPaged(PageRequest pageRequest) {
+        var movies = repository.findAll(pageRequest);
+        return movies.map(MovieDTO::new);
     }
 }
