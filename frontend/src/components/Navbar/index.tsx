47df48f83ebed98b7e41d2590a08d8eec3fa80ca
Raphael Fontoura <raphael.fontoura@gmail.com>
2021-11-18 10:18:11 -0300

    Start architecture. Start Navbar

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,16 @@
+import "./styles.css";
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <h1>MovieFlix</h1>
+      </div>
+      <div className="navbar-logout">
+
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
