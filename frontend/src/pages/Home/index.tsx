a149c8b32201288aed46b014cb5117d90b89e946
Raphael Fontoura <raphael.fontoura@gmail.com>
2021-11-18 11:32:49 -0300

    Home page left side custom

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,11 +1,31 @@
-import React from 'react'
-import Navbar from '../../components/Navbar'
+import Navbar from '../../components/Navbar';
+import ImageHome from 'assets/img/home-image.svg';
+import "./styles.css";
 
 function Home() {
   return (
-    <div>
+    <>
       <Navbar />
-    </div>
+      <div className="home-container">
+        <div className="home-presentation">
+          <h1 className="home-presentation-title">
+            Avalie Filmes
+          </h1>
+          <p className="home-presentation-text">
+            Diga o que você achou do seu filme favorito
+          </p>
+          <img src={ImageHome} 
+            alt="Imagem da página Home" 
+            className="home-presentation-img" />
+        </div>
+        <div className="login-form">
+          <h1 className="login-title">Login</h1>
+          <input type="text" className="login-input" />
+          <input type="password" className="login-input" />
+          <button className="login-button" type="button">Login</button>
+        </div>
+      </div>
+    </>
   )
 }
 
